78f8a49bcbd462052b277d62e85f425008e0622f
Iskren Hadzhinedev <iskren.hadzhinedev@ikiji.com>
2015-02-04 03:16:38 +0200

    Cleanup the ViewController, added EventSource polyfill

--- js/netfm/main.js
+++ js/netfm/main.js
@@ -1,13 +1,19 @@
 (function() {
     var app = angular.module("NetFM", []);
 
-    Tab = function(path) {
-        this.path == path || [];
+    var Tab = function(path) {
+        this.path = path || "";
+        this.breadcrumb = this.path.split("/");
         
+        // Remove blank elements generated from absolute paths
+        for (var ind = this.breadcrumb.indexOf(""); ind != -1;ind = this.breadcrumb.indexOf("")) {
+            this.breadcrumb.splice(ind, 1);
+        }
+
         this.basename = function() {
             if (!this.path)
                 return;
-            return this.path[this.path.length -1];
+            return this.path[this.breadcrumb.length -1];
         };
         
         this.realpath = function() {
@@ -15,20 +21,18 @@
                 return;
             this.path.slice(0,-1).join("/");
         };
+        
+        this.content = [
+                new Entry('Downloads', 'folder'),
+                new Entry('Music', 'folder'),
+                new Entry('public_html', 'folder')
+            ];
     };
-
-    app.controller('BreadcrumbController', function() {
-        this.path = ['home','axtroz','projects','netfm','client'];
-        this.cwd = "";
-
-        this.chdir = function(dir) {
-            console.log('switching to ' + dir);
-            if (this.path.indexOf(dir) === -1) {
-                this.path.push(dir);
-            }
-            this.cwd = dir;
-        };
-    });
+    
+    var Entry = function(name, type) {
+        this.name = name || "";
+        this.type = type || "unknown";
+    };
 
     app.controller('BookmarkController', function() {
         this.bookmarks = ['home', 'www'];
@@ -47,17 +51,21 @@
 
     app.controller('ViewController', function() {
         this.tabs = [(new Tab('/'))];
+        tabs = this.tabs
         this.currentTab = this.tabs[0];
+        
         this.addTab = function(path) {
+            if (!path)
+                path = this.currentTab.path
             var tab = new Tab(path);
             this.tabs.push(tab);
         };
 
         this.closeTab = function(tab) {
-            var tabIndex = this.tabs.indexOf(tab)
-            if (tabIndex == this.currentTab)
-                this.currentTab--;
+            var tabIndex = this.tabs.indexOf(tab);
             this.tabs.splice(tabIndex, 1);
+            if (tab == this.currentTab)
+                this.currentTab = this.tabs[0];
         };
 
         this.setTab = function(tab) {
